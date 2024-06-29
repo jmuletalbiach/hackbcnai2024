8098dafff597278789833cfaa12accaa2735223a
Ricard Mulet Alarcon <rmuletalarcon@gmail.com>
2024-06-29 22:33:00 +0200

    docs ref

--- api_mistral.py
+++ api_mistral.py
@@ -70,14 +70,14 @@ def search(Item:Item):
     i = 0
     list_res = []
     for res in search_result:
-        list_res.append({"id":i,"path":res.metadata.get("path"),"content":res.page_content})
+        list_res.append({"id":i,"path":res.metadata.get("filename"),"content":res.page_content})
     return list_res
 
 @app.post("/query_ai")
 async def query_ai(Item:Item):
     query = Item.query
     search_result = qdrant.similarity_search(
-        query=query, k=2 #seran 10
+        query=query, k=10 #seran 10
     )
     i = 0
     list_res = []
@@ -86,8 +86,8 @@ async def query_ai(Item:Item):
     i = 0
     for res in search_result:
         context = context + str(i)+"\n"+res.page_content+"\n\n"
-        mappings[i] = res.metadata.get("path")
-        list_res.append({"id":i,"path":res.metadata.get("path"),"content":res.page_content})
+        mappings[i] = res.metadata.get("filename")
+        list_res.append({"id":i,"path":res.metadata.get("filename"),"content":res.page_content})
         i = i +1
 
     rolemsg = {"role": "system",
@@ -95,14 +95,16 @@ async def query_ai(Item:Item):
     messages = [
         rolemsg,
         {"role": "user", "content": "Documents:\n"+context+"\n\nQuestion: "+query},
-       ## {"role": "user", "content": "Capital of France"},
     ]
+    
+    
+   
     print (messages)
     chat_response = client_LLM.chat(
     model=model,
     messages = [
-    ChatMessage(role="system", content="Eres un profesional de la educación social, que trabaja con niños y jóvenes en situación de exclusión social. Tu objetivo es diseñar actividades que potencien la educación emocional, la educación sexoafectiva i interseccional, los cuidados para favorecer la corresponsabilidad en las tareas de la vida diária y la diversidad cultural. Estas actividades deben de ser inclusivas, accesibles y adaptadas a las realidades y a las edades de estos usarios, fomentando un entorno seguro y de respeto. \\n Objetivos:\\n Objetivo de la Educación emocional: Facilitar la comprensión y gestión de las emociones propias y ajenas, desarrollando habilidades de empatia, autorregulación emocional y comunicación asertiva. \\n Objetivo de la Educación sexoafectiva i interseccional: Promover una comprensión integral y respetuosa de la sexualidad y las relaciones afectivas, abordando temas como el género, el consentimiento, los etereotipos y roles de género, la orientación afectivosexual y el respeto mutuo a la diversidad sexoafecitva. \\n Objetivo de los cuidados: Fomentar practicas de cuidados mutuos y autocuidados a partir de la corresponsabilidad, destacando la importancia del bienestar físico y mental. \\n Objetivo de la Diversidad cultural: Fomentar el reconocimiento y el respeto de la diversidad cultural, promoviendo el respeto y la inclusión de la interculturalidad. \\n  Las edades de los niños y jóvenes con los que trabajamos son de infancia de 6 a 9 años, de 10 a 12 años, jóvenes de 12 a 16 años. \\n"),    
-    ChatMessage(role="user", content="Documents:\n"+context+"\n\nQuestion: "+query)
+    ChatMessage(role="system", content="Answer always user's question using documents given in the section [DOCUMENTS-CONTEXT]. In the context are documents that should contain an answer. Please always reference document id (in squere brackets, for example [0],[1]) of the document that was used to make a claim. Use as many citations and documents as it is necessary to answer question. Question is indicated in the section [QUESTION]\n Eres un profesional de la educación social, que trabaja con niños y jóvenes en situación de exclusión social. Tu objetivo es diseñar actividades que potencien la educación emocional, la educación sexoafectiva i interseccional, los cuidados para favorecer la corresponsabilidad en las tareas de la vida diária y la diversidad cultural. Estas actividades deben de ser inclusivas, accesibles y adaptadas a las realidades y a las edades de estos usarios, fomentando un entorno seguro y de respeto. \n Objetivos:\n Objetivo de la Educación emocional: Facilitar la comprensión y gestión de las emociones propias y ajenas, desarrollando habilidades de empatia, autorregulación emocional y comunicación asertiva. \n Objetivo de la Educación sexoafectiva i interseccional: Promover una comprensión integral y respetuosa de la sexualidad y las relaciones afectivas, abordando temas como el género, el consentimiento, los etereotipos y roles de género, la orientación afectivosexual y el respeto mutuo a la diversidad sexoafecitva. \n Objetivo de los cuidados: Fomentar practicas de cuidados mutuos y autocuidados a partir de la corresponsabilidad, destacando la importancia del bienestar físico y mental. \\n Objetivo de la Diversidad cultural: Fomentar el reconocimiento y el respeto de la diversidad cultural, promoviendo el respeto y la inclusión de la interculturalidad. \n  Las edades de los niños y jóvenes con los que trabajamos son de infancia de 6 a 9 años, de 10 a 12 años, jóvenes de 12 a 16 años. \n"),    
+    ChatMessage(role="user", content=" [DOCUMENTS-CONTEXT]\n"+context+"\n\n[Question] "+query)
     ]
     )
     response = chat_response.choices[0].message.content
